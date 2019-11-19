745378c0ba64dc9388dd5874cbd4c3fbd6f8d18c
Arief Kurniawan <45711484+raikouraza@users.noreply.github.com>
2019-11-19 07:25:46 +0700

    masih membuat form, grafik kehadiran beserta tabelnya
    masih melakukan perbaikan grafik, form, dan tabel

--- KP/GKP_KP/DAO/KehadiranDaoImpl.php
+++ KP/GKP_KP/DAO/KehadiranDaoImpl.php
@@ -3,5 +3,81 @@
 
 class KehadiranDaoImpl
 {
+    public function getAllKehadiran()
+    {
+        $link = PDOUtility::get_koneksi();
+        $link->beginTransaction();
+        $query = "SELECT * FROM tbkehadiran ORDER BY Kehadiran_Id";
+        $result = $link->query($query);
+        $result->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE,"Kehadiran");
+        $link = null;
+        return $result;
+    }
 
+    public function addKehadiran(Kehadiran $kehadiran)
+    {
+        $link = PDOUtility::get_koneksi();
+        $link->beginTransaction();
+        $query = "INSERT INTO tbkehadiran(Kehadiran_Id, Kehadiran_Tanggal, Kehadiran_Jumlah_Wanita, Kehadiran_Jumlah_Pria, Kehadiran_Jumlah_Persembahan, Kehadiran_Gereja_Id) VALUES (?)";
+        $statement = $link->prepare($query);
+        $statement->bindValue(1, $kehadiran->getKehadiranId(), PDO::PARAM_STR);
+        $statement->bindValue(2, $kehadiran->getKehadiranTanggal(), PDO::PARAM_STR);
+        $statement->bindValue(3, $kehadiran->getKehadiranJumlahWanita(), PDO::PARAM_INT);
+        $statement->bindValue(4, $kehadiran->getKehadiranJumlahPria(), PDO::PARAM_INT);
+        $statement->bindValue(5, $kehadiran->getKehadiranJumlahPersembahan(), PDO::PARAM_INT);
+        $statement->bindValue(6, $kehadiran->getKehadiranGereja(), PDO::PARAM_STR);
+        if ($statement->execute()) {
+            $link->commit();
+        } else {
+            $link->rollBack();
+        }
+        $link = null;
+    }
+
+    function deleteKehadiran(Kehadiran $kehadiran)
+    {
+        $link = PDOUtility::get_koneksi();
+        $link->beginTransaction();
+        $query = "DELETE FROM tbkehadiran WHERE Kehadiran_Id = ?";
+        $statement = $link->prepare($query);
+        $statement->bindValue(1,$kehadiran->getKehadiranId() , PDO::PARAM_STR);
+        if ($statement->execute()) {
+            $link->commit();
+        } else {
+            $link->rollBack();
+        }
+        $link = null;
+    }
+
+    function updateKehadiran(Kehadiran $kehadiran)
+    {
+        $link = PDOUtility::get_koneksi();
+        $link->beginTransaction();
+        $query = 'UPDATE tbkehadiran SET Kehadiran_Tanggal = ?, Kehadiran_Jumlah_Wanita = ?, Kehadiran_Jumlah_pria = ?, Kehadiran_Jumlah_Persembahan = ?, Kehadiran_Gereja_id = ?  WHERE Kehadiran_Id = ?';
+        $statement = $link->prepare($query);
+        $statement->bindValue(1, $kehadiran->getKehadiranTanggal(), PDO::PARAM_STR);
+        $statement->bindValue(2, $kehadiran->getKehadiranJumlahWanita(), PDO::PARAM_INT);
+        $statement->bindValue(3, $kehadiran->getKehadiranJumlahPria(), PDO::PARAM_INT);
+        $statement->bindValue(4, $kehadiran->getKehadiranJumlahPersembahan(), PDO::PARAM_INT);
+        $statement->bindValue(5, $kehadiran->getKehadiranGereja(), PDO::PARAM_INT);
+        $statement->bindValue(6, $kehadiran->getKehadiranId(), PDO::PARAM_STR);
+        if ($statement->execute()) {
+            $link->commit();
+        } else {
+            $link->rollBack();
+        }
+        $link = null;
+    }
+
+    function getKehadiran($id)
+    {
+        $link = PDOUtility::get_koneksi();
+        $query = "SELECT * FROM tbkehadiran WHERE Kehadiran_Id = ? LIMIT 1";
+        $statement = $link->prepare($query);
+        $statement->bindParam(1, $Kehadiran_Id, PDO::PARAM_STR);
+        $statement->execute();
+        $result = $statement->fetchObject('Kehadiran');
+        $link = null;
+        return $result;
+    }
 }
